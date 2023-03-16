2e2c4164c5ea73556f97fe7d32c26ae480f8d5c0
Luka Kikelj <kikelj.luka@gmail.com>
2023-03-16 11:17:10 +0100

    use another vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { createVuePlugin } from "vite-plugin-vue2";
+import vue from "@vitejs/plugin-vue2";
 
 export default {
-  plugins: [createVuePlugin(/* options */)],
+  plugins: [vue()],
 };
