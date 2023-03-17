0f08181580f4f6a6ca6f7f25560f2441cf86f6cc
Luka Kikelj <kikelj.luka@gmail.com>
2023-03-17 18:13:41 +0100

    add fetch function and show the list of all breeds

--- src/components/BreedsList.vue
+++ src/components/BreedsList.vue
@@ -1,13 +1,45 @@
 <template>
   <section>
-    <h2>{{ name }}</h2>
+    <div v-for="breed in breedsList">
+      <h3>{{ breed.name }}</h3>
+      <span>{{ breed.breed_group }}</span>
+      <span>{{ breed.life_span }}</span>
+      <img
+        class="breedsList_image"
+        :src="breed.image.url"
+        :alt="`Picture of the ${breed.name} dog`"
+      />
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   props: ["name"],
+  data() {
+    return {
+      breedsList: [],
+    };
+  },
+  methods: {
+    fetchAllData() {
+      fetch(`https://api.thedogapi.com/v1/breeds/`)
+        .then((response) => response.json())
+        .then((results) => {
+          this.breedsList = results;
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+  mounted() {
+    this.fetchAllData();
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.breedsList_image {
+  width: 20rem;
+  height: auto;
+}
+</style>
