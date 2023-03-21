eae4f38414f1ce75f57aea3e854882f108ad1310
Luka Kikelj <kikelj.luka@gmail.com>
2023-03-21 11:51:48 +0100

    add some v-else-if statement

--- src/components/BreedsList.vue
+++ src/components/BreedsList.vue
@@ -1,37 +1,60 @@
 <template>
   <section>
-    <div v-for="breed in breedsList">
-      <h3>{{ breed.name }}</h3>
-      <span>{{ breed.breed_group }}</span>
-      <span>{{ breed.life_span }}</span>
-      <img
-        class="breedsList_image"
-        :src="breed.image.url"
-        :alt="`Picture of the ${breed.name} dog`"
-      />
+    <p v-if="isLoading">The Data is Loading ...</p>
+    <p v-else-if="!isLoading && error">{{ error }}</p>
+    <p v-else-if="!isLoading && breedsList.length === 0">
+      No Data Found. Please try again later.
+    </p>
+    <div v-else>
+      <div v-for="breed in breedsList">
+        <h3>{{ breed.name }}</h3>
+        <span>{{ breed.breed_group }}</span>
+        <span>{{ breed.life_span }}</span>
+        <span>{{ breed.height.metric }}cm</span>
+        <span>{{ breed.weight.metric }}kg</span>
+        <img
+          class="breedsList_image"
+          :src="breed.image.url"
+          :alt="`Picture of the ${breed.name} dog`"
+        />
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  props: ["name"],
   data() {
     return {
       breedsList: [],
+      isLoading: false,
+      error: null,
     };
   },
   methods: {
     fetchAllData() {
+      this.isLoading = true;
+      this.error = null;
       fetch(`https://api.thedogapi.com/v1/breeds/`)
-        .then((response) => response.json())
-        .then((results) => {
-          this.breedsList = results;
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
         })
-        .catch((error) => console.log(error));
+
+        .then((results) => {
+          this.isLoading = false;
+          if (results.length > 0) {
+            this.breedsList = results;
+          }
+        })
+        .catch((error) => {
+          this.isLoading = false;
+          this.error = "Failed to fetch data. Please try again later!";
+        });
     },
   },
-  mounted() {
+  created() {
     this.fetchAllData();
   },
 };
