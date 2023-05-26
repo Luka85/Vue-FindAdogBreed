c7e7034bbea01c24b05993462e19ce2d199f2c74
Luka Kikelj <kikelj.luka@gmail.com>
2023-05-26 14:56:47 +0200

    add scrolling functionality from feature-Details branch

--- src/components/BreedsList.vue
+++ src/components/BreedsList.vue
@@ -1,6 +1,7 @@
 <template>
-  <section>
+  <section ref="breedListRef" class="breedsList__container">
     <search-form
+      ref="searchRef"
       :isDisabled="isInputDisabled"
       @search="getSearchResults"
     ></search-form>
@@ -22,11 +23,18 @@
           :breed="breed"
           :key="breed.id"
           :id="index"
-        ></breed-card>
+          @toggle="toggleCard"
+          :class="{ expandble: breed.isActive, result__list: !breed.isActive }"
+        >
+          <template v-slot:default>
+            <router-view :breed="breed"></router-view>
+            <!-- <breed-details :breed="breed"></breed-details> -->
+          </template>
+        </breed-card>
       </ul>
     </div>
     <navigation-button
-      @clickToScroll="scrollToTop"
+      @clickToScroll="scrollListToTop"
       :class="{ hidden: isHidden ? 'hidden' : '' }"
     ></navigation-button>
   </section>
@@ -38,11 +46,20 @@ import SearchForm from "./SearchForm.vue";
 import { searchBreed } from "../data";
 import BreedCard from "./BreedCard.vue";
 import NavigationButton from "./NavigationButton.vue";
+import BreedDetails from "./BreedDetails.vue";
 export default {
   components: {
     SearchForm,
     BreedCard,
     NavigationButton,
+    BreedDetails,
+  },
+
+  props: {
+    breedName: {
+      type: String,
+      default: "",
+    },
   },
 
   data() {
@@ -55,6 +72,7 @@ export default {
       isInputDisabled: false,
       isHidden: true,
       message: "",
+      isScrollToTopActive: false,
     };
   },
   methods: {
@@ -97,12 +115,18 @@ export default {
         .then((results) => {
           this.isLoading = false;
           if (results.length > 0) {
-
+            this.message = "";
             this.searchList = results.map((breed) => {
               breed.isActive = false;
               return breed;
             });
-
+            // console.log(this.searchList);
+          } else if (results.length === 1) {
+            results[0].isActive = true;
+            this.searchList = results;
+            // console.log(this.searchList);
+            // console.log(this.$route);
+            return this.searchList;
             this.message = "";
           } else {
             this.searchList = [];
@@ -117,26 +141,65 @@ export default {
         .catch((error) => {
           this.isLoading = false;
           this.error = error.name + ": " + error.message;
+          this.isInputDisabled = true;
         });
     },
 
     showNavigationBtn() {
-      const resultListContainer = this.$refs.resultListContainer;
-
-      if (resultListContainer.scrollTop > 450) {
+      if (this.$refs.resultListContainer.scrollTop > 450) {
         this.isHidden = false;
+        this.isScrollToTopActive = true;
       } else {
         this.isHidden = true;
       }
     },
-    scrollToTop() {
-      this.$refs.resultListContainer.scrollTo({ top: 0, behavior: "smooth" });
+    scrollListToTop() {
+      this.isScrollToTopActive = true;
+      this.$refs.resultListContainer.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+      this.displayedBreeds.forEach((breed) => {
+        breed.isActive = false;
+      });
+      if (this.$route.name !== "breeds") {
+        this.$router.push({
+          name: "breeds",
+        });
+      }
     },
     toggleCard(breed) {
+      console.log(breed.isActive, breed.name, this.$route.name);
       breed.isActive = !breed.isActive;
       this.displayedBreeds.forEach((item) => {
-        if (item.id !== breed.id) item.isActive = false;
+        if (item.name !== breed.name) {
+          item.isActive = false;
+        }
       });
+      if (!breed.isActive) {
+        console.log("push");
+        this.$router.push({
+          name: "breeds",
+        });
+      }
+    },
+    scrollToBreedCard(breedName) {
+      const breedNameParam = breedName.params.breedName;
+      console.log("scrollToBreedCard method");
+      console.log("breedName:", breedNameParam);
+
+      if (breedNameParam) {
+        this.displayedBreeds.filter((breed, id) => {
+          if (breedNameParam.toLowerCase() === breed.name.toLowerCase()) {
+            this.$nextTick(() => {
+              this.$refs.resultListContainer.children[id].scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+              });
+            });
+          }
+        });
+      }
     },
   },
   computed: {
@@ -156,14 +219,45 @@ export default {
       }
     },
     receivedDataState() {
-      if (!this.displayedBreeds.length) {
+      if (!this.displayedBreeds.length && !this.isLoading) {
         return this.message;
       }
+    },
+    expandbleClass() {
+      this.displayedBreeds.forEach((breed) =>
+        breed.isActive ? "expandble" : "result__list"
+      );
+    },
+  },
+  watch: {
+    $route(newRoute, oldRoute) {
+      console.log("WATCH ROUTE", "new", newRoute, "old", oldRoute);
+
+      if (this.$route.name === "breedName") {
+        this.$router.push({
+          name: "details",
+        });
+      }
+
+      this.scrollToBreedCard(newRoute);
     },
   },
 
   created() {
+    console.log("created");
     this.fetchData();
+  },
+
+  updated() {
+    console.log("updated hook");
+
+    this.scrollToBreedCard(this.$route);
+
+    if (this.$route.name === "breedName") {
+      this.$router.push({
+        name: "details",
+      });
+    }
   },
 };
 </script>
@@ -172,9 +266,9 @@ export default {
 .result__list-container {
   list-style-type: none;
   padding: 0;
-  max-height: 120vh;
-  overflow-x: hidden;
-  overflow-y: auto;
+  max-height: 70vh;
+  overflow-y: scroll;
+  margin-bottom: 3rem;
 }
 
 p {
@@ -194,5 +288,22 @@ ul::-webkit-scrollbar {
   text-align: center;
   background-color: #f2e3dbad;
   color: var(--color-primary);
+}
+
+.expandble {
+  padding: 4rem 3rem;
+  box-shadow: 15px 5px 8px 0px #6264728f;
+  padding: 1.5rem 4rem;
+}
+.result__details {
+  margin-top: 3rem;
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 0 4rem;
+}
+.hidden {
+  display: none;
 }
 </style>
