d03e0f0785e14ce97116eb1a4857b10bd94dd24f
Luka Kikelj <kikelj.luka@gmail.com>
2023-06-06 10:26:41 +0200

    change base in vite config file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import vue from "@vitejs/plugin-vue2";
 
 export default {
   plugins: [vue()],
-  base:
-    process.env.NODE_ENV === "production" ? "/Vue-FindAdogBreed/breeds" : "/",
+
+  base: "/Vue-FindAdogBreed/breeds",
 };
