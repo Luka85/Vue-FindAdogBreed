b3cc76a3d7f24bd517f16b50a7b13a861c99619f
Luka Kikelj <kikelj.luka@gmail.com>
2023-06-06 08:51:52 +0200

    fix the vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 //   base: "/Vue-FindAdogBreed/",
 // };
 
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/Vue-FindAdogBreed/" : "/",
-};
+// module.exports = {
+//   publicPath:
+//     process.env.NODE_ENV === "production" ? "/Vue-FindAdogBreed/" : "/",
+// };
