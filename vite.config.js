4238570708756e18d4d07f0bb531476365f9ddd9
Luka Kikelj <kikelj.luka@gmail.com>
2023-06-07 15:09:38 +0200

    change vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import vue from "@vitejs/plugin-vue2";
 
-export default {
+// export default {
+
+// };
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
   base: "/Vue-FindAdogBreed/",
   plugins: [vue()],
-};
+});
