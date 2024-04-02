57f8f89778400666959575b87e35563bc92a2c5d
Luka Kikelj <kikelj.luka@gmail.com>
2024-04-02 16:45:16 +0200

    modified connect-history-api-fallback inside server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,14 @@ app.use(
     verbose: true,
   })
 );
-app.use("/", express.static(path.join(__dirname, "client/dist")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use((req, res, next) => {
+    res.status(200).sendFile(path.join(__dirname + "/client/dist/index.html"));
+  });
+}
+// app.use("/", express.static(path.join(__dirname, "client/dist")));
 app.use(cors());
 app.use("/auth", authRouter);
 
